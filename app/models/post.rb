035b33f9e75e0537d559fc22f52607e7e50f7751
dlonra22 <arnold.bwaila@gmail.com>
2020-09-12 16:40:27 +0100

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+    CATEGORY_TYPES = ["Fiction","Non-Fiction"]
+    validates :title, presence: true
+    validates :category, inclusion: {in: CATEGORY_TYPES}
+    validates :content, length:{minimum: 100}
 end
